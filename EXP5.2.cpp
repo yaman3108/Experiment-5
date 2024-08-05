47d2ce9e63b1d88ec0dbe399c70c246db5eff39d
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-08-05 11:15:29 +0530

    Create EXP5.2.cpp

---
+++ EXP5.2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+// Nested if else program
+int main() {
+    int  num1, num2, num3;
+    cout<<"Enter number-1: ";
+    cin>>num1;
+    cout<<"Enter number-2: ";
+    cin>>num2;
+    cout<<"Enter number-3: ";
+    cin>>num3;
+
+    if(num1>=num2) {
+        if(num1>=num3) {
+            cout<<num1<<" is the greatest number"<<endl;
+        }
+        else {
+            cout<<num3<<" is the greatest number"<<endl;
+        }
+    }
+    else if(num2>=num1) {
+        if(num2>=num3) {
+            cout<<num2<<" is the greatest number"<<endl;
+        }
+        else {
+            cout<<num3<<" is the greatest number"<<endl;
+        }
+    }
+    
+}
